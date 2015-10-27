e05934906a140f8341c1edbd041e5bcdf77593c0
SilenceWang <silencelikesky@sina.com>
2015-10-27 14:21:07 +0800

    修改

---
+++ css/test.py
@@ -0,0 +1,19 @@
+#! /bin/python
+#! coding:utf-8
+
+no_tallage = ['book','food','headeach pills']
+
+def base_tallage(product, product_price):
+    pixels = 0.1
+    if products not in no_tallage:
+        return float(pixels) * product_price + product_price
+    return product_price
+
+def import_tallage(products):
+    pixels = 0.5
+    return float(pixels) * product_price + product_price
+
+raw_input()
+
+
+/webprolist.do?data={"uid":"44194901","screen_width":"1080","app_version":"1.8.2.0","device_id":"5540FEDE-DB51-4A0D-929B-2387EB1CAD86","net":"WIFI","country_code":"CN","os_version":"4.4.4","os_type":"ios","appvc":"1080200","bssid":"3c:8c:40:c6:fa:50","device_name":"M355","pnum":"13717528295","screen_layout_size":"2","app_package_name":"com.happy.lock","ssid":"dianjoy","app_id":"0","screen_density":"400","pw":"d9ac064568bc845a0de885e2d8ddd54b","language":"zh","imsi":"460025012436898","channel":"share","re_time":"1441691926261","screen_height":"1800"}
